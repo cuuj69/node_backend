d56db0f91a4ee5d074a0b2910846406f3477ea7e
cuuj69 <mensahjefferson69@gmail.com>
2023-08-05 16:12:36 +0000

    Modified:entrypoint(myApp.js)

--- myApp.js
+++ myApp.js
@@ -2,9 +2,22 @@ let express = require('express');
 let app = express();
 
 
+//log Hello World to the console
+//console.log("Hello World");
+
+//send Hello Express to the node server
+//app.get('/',((req,res)=>{
+//	res.send("Hello Express")
+
+//}))
 
 
+//send HTML file
 
+app.get('/',((req,res)=>{
+	res.sendFile(__dirname + '/views/index.html')
+
+}))
 
 
 
