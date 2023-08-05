73a5d9fe3dc56b6a5f39c73c98a619aa29607dd5
cuuj69 <mensahjefferson69@gmail.com>
2023-08-05 21:47:04 +0000

    Modified:code(implement root logger)

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-
+require('dotenv').config()
 
 //log Hello World to the console
 //console.log("Hello World");
@@ -12,13 +12,33 @@ let app = express();
 //}))
 
 
-//send HTML file
+//root level logger
 
+
+app.use((req,res,next)=>{
+	console.log(`${req.method} ${req.path} - ${req.ip}`)
+	next()
+})
+
+//mount assets on specific route
+app.use('/public', express.static(__dirname + '/public'))
+
+// serve html file
 app.get('/',((req,res)=>{
 	res.sendFile(__dirname + '/views/index.html')
 
 }))
 
+
+//serving json file
+//accessing .env files to effect changes to uri/urls
+app.get('/json',((req,res)=>{
+	let response = "Hello json"
+	if(process.env.MESSAGE_STYLE == 'uppercase'){
+		res.json({"message":response.toUpperCase()});
+	} else 
+	{res.json({"message":response}) }
+}))
 
 
 
